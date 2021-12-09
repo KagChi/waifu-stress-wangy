c2aeb55ea0bb2b4f7bc8d79c2c1643e603d12e83
KChi <59391215+KagChi@users.noreply.github.com>
2021-12-09 06:27:51 +0000

    feat: custom 404 pages with keqing YES!

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -17,32 +17,30 @@ const Page404 = () => {
   return (
     <>
       <MotionBox
-        animate={{ y: 20 }}
+        animate={{ y: 20, scale: 0.97 }}
         transition={{ repeat: Infinity, duration: 2, repeatType: "reverse" }}
-        width={["100%", "70%", "60%", "60%"]}
-        margin="0 auto"
+        marginY={8}
+        maxWidth={200}
+        marginX="auto"
       >
         <Image
-          src="/404 Error-pana.svg"
-          alt="Error 404 not found Illustration"
+				  width={200}
+          height={200}
+          src={`/${colorMode}_keqing_loss.svg`}
+          alt="Keqing LOSS"
         />
       </MotionBox>
-      <Text textAlign="center" fontSize="xs">
-        <ChakraLink href="https://stories.freepik.com/web" isExternal>
-          Illustration by Freepik Stories
-        </ChakraLink>
-      </Text>
 
       <Box marginY={4}>
-        <Heading textAlign="center">Page not Found.</Heading>
+        <Heading textAlign="center">404 | Kamu tersesat.</Heading>
 
         <Box textAlign="center" marginTop={4}>
-          <Text>It&apos;s Okay!</Text>
+          <Text>Tidak Apa apa !</Text>
           <Link href="/" passHref>
             <Button
               backgroundColor={colorMode === "light" ? "gray.300" : "teal.500"}
             >
-              Let&apos;s Head Back
+              Ayo kembali
             </Button>
           </Link>
         </Box>
